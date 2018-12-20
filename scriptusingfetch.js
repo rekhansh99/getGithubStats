127463f268cc7838c2cf06db24afb60732e7d2f1
Rekhansh Priyadarshi <42772481+rekhansh99@users.noreply.github.com>
2018-12-20 15:56:44 +0530

    resolved issue in scriptusingfetch.js

--- scriptusingfetch.js
+++ scriptusingfetch.js
@@ -13,19 +13,19 @@ fetch(url1)
        reponame[x] = data[x].name; 
        console.log(reponame[x]);
     }
-});
-
-for(let x=0;x<reponame.length;x++) //to find weekly commits
-{
-    const tempurl = url + reponame[x] +"/stats/participation";
-    fetch(tempurl)
-    .then(function(response){
-         return response.json();
-    })
-    .then(function(data){
-    var owns = data.owner;
-    for(let y=0;y<owns.length;y++){
-       week[y] += owns[y]; 
+}).then(function() {
+    for(let x=0;x<reponame.length;x++) //to find weekly commits
+    {
+        const tempurl = url + reponame[x] +"/stats/participation";
+        fetch(tempurl)
+        .then(function(response){
+             return response.json();
+        })
+        .then(function(data){
+        const owns = data.owner;
+        for(let y=0;y<owns.length;y++){
+           week[y] += owns[y]; 
+        }
+        });
     }
-    });
-}
+});
